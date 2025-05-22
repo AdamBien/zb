260f4819738d8a1f3c7167df2466333e8180ea50
Adam Bien <abien@adam-bien.com>
2025-05-22 14:35:34 +0200

    compiler integrated with file discovery

--- src/main/java/airhacks/zb/compiler/control/Compiler.java
+++ src/main/java/airhacks/zb/compiler/control/Compiler.java
@@ -7,13 +7,21 @@ import java.nio.file.Path;
 import javax.tools.Tool;
 import javax.tools.ToolProvider;
 
+import airhacks.zb.discovery.control.JavaFiles;
+
 interface Compiler {
 
   static boolean compile(Path rootDirectory) {
+    var javaFiles =  JavaFiles.findFrom(rootDirectory);
+    return compile(javaFiles);
+  }
+
+  static boolean compile(List<Path> javaFiles) {
     var javac = ToolProvider.getSystemJavaCompiler();
     var fm = javac.getStandardFileManager(null, null, null);
-    var cus = fm.getJavaFileObjects(rootDirectory);
+    var cus = fm.getJavaFileObjectsFromPaths(javaFiles);
     var task = javac.getTask(null, fm, null, null, null, cus);
     return task.call();
+
   }
 }
