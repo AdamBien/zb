bc4f98aaccaab2efca5b38942d7c08e8c6856ccb
Adam Bien <abien@adam-bien.com>
2025-05-23 18:06:07 +0200

    initial flow implementation

--- src/main/java/airhacks/App.java
+++ src/main/java/airhacks/App.java
@@ -1,4 +1,9 @@
 package airhacks;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import airhacks.zb.compiler.control.Compiler;
+import airhacks.zb.packer.control.Packer;
 
 
 /**
@@ -7,7 +12,12 @@ package airhacks;
  */
 interface App {
 
-    static void main(String... args) {
-        
+    static void main(String... args) throws IOException {
+        var sourceDirectory = Path.of("src/main/java");
+        var classesDirectory =  Path.of("target/test/classes");
+        var jarDirectory = Path.of("target/test/jar");
+        var jarFileName = "test.jar";
+        Compiler.compile(sourceDirectory,classesDirectory);
+        Packer.archive(classesDirectory, jarDirectory, jarFileName);
     }
 }
