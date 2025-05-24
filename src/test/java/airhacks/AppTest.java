9543d47df8e930b6eb820eac840e6422eed2f71d
Adam Bien <abien@adam-bien.com>
2025-05-24 05:43:23 +0200

    AppTest unit test created

---
+++ src/test/java/airhacks/AppTest.java
@@ -0,0 +1,44 @@
+package airhacks;
+
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void argumentsWithDefaultValues() {
+        var arguments = App.Arguments.from();
+        
+        assertThat(arguments.sourceDirectory()).isEqualTo(Path.of("src/main/java"));
+        assertThat(arguments.classesDirectory()).isEqualTo(Path.of("target/test/classes"));
+        assertThat(arguments.jarDirectory()).isEqualTo(Path.of("target/test/jar"));
+        assertThat(arguments.jarFileName()).isEqualTo("test.jar");
+    }
+
+    @Test
+    void argumentsWithCustomValues() {
+        var arguments = App.Arguments.from(
+            "custom/src",
+            "custom/classes",
+            "custom/jar",
+            "custom.jar"
+        );
+        
+        assertThat(arguments.sourceDirectory()).isEqualTo(Path.of("custom/src"));
+        assertThat(arguments.classesDirectory()).isEqualTo(Path.of("custom/classes"));
+        assertThat(arguments.jarDirectory()).isEqualTo(Path.of("custom/jar"));
+        assertThat(arguments.jarFileName()).isEqualTo("custom.jar");
+    }
+
+    @Test
+    void argumentsWithPartialCustomValues() {
+        var arguments = App.Arguments.from("custom/src");
+        
+        assertThat(arguments.sourceDirectory()).isEqualTo(Path.of("custom/src"));
+        assertThat(arguments.classesDirectory()).isEqualTo(Path.of("target/test/classes"));
+        assertThat(arguments.jarDirectory()).isEqualTo(Path.of("target/test/jar"));
+        assertThat(arguments.jarFileName()).isEqualTo("test.jar");
+    }
+} 
