07926b1d45e36b3a1501890d997a98f6ddb2b1cf
Adam Bien <abien@adam-bien.com>
2025-06-01 19:13:17 +0200

    jar removed before creation, manifest added

--- src/main/java/airhacks/zb/packer/control/Packer.java
+++ src/main/java/airhacks/zb/packer/control/Packer.java
@@ -11,22 +11,27 @@ import java.util.jar.JarOutputStream;
 
 public interface Packer {
 
-
-    static void archive(Path rootClassesDirectory, Path rootJARDirectory, String jarFileName,Optional<Path> mainClass) throws IOException {
+    static void archive(Path rootClassesDirectory, Path rootJARDirectory, String jarFileName, Optional<Path> mainClass) throws IOException {
         var jarFile = rootJARDirectory.resolve(jarFileName);
         Files.createDirectories(rootJARDirectory);
-        try (var fos = Files.newOutputStream(jarFile,StandardOpenOption.CREATE);
-                var jos = new JarOutputStream(fos)) {
+        Files.deleteIfExists(jarFile);
+        
+        try (var fos = Files.newOutputStream(jarFile, StandardOpenOption.CREATE_NEW);
+             var jos = new JarOutputStream(fos)) {
+            
+            // Add manifest first if main class is present
+            mainClass.ifPresent(mc -> addManifest(rootClassesDirectory, jos, mc));
+            
+            // Then add all class files
             try (var paths = Files.walk(rootClassesDirectory)) {
                 paths.filter(path -> path.toString().endsWith(".class"))
-                        .forEach(path -> addEntry(rootClassesDirectory, jos, path));
+                     .forEach(path -> addEntry(rootClassesDirectory, jos, path));
             }
-           mainClass.ifPresent(mc -> addManifest(rootClassesDirectory, jos, mc));
         }
     }
 
-    static void addManifest(Path rootClassesDirectory, JarOutputStream jos,Path mainClass)  {
-        var javaPackage =  pathToJavaPackage(mainClass);
+    static void addManifest(Path rootClassesDirectory, JarOutputStream jos, Path mainClass) {
+        var javaPackage = pathToJavaPackage(mainClass);
         var fullyQualifiedClassName = javaPackage.replace(".java", "");
         var manifest = Manifestor.manifest(fullyQualifiedClassName);
         try {
@@ -39,24 +44,20 @@ public interface Packer {
         }
     }
 
-    static String pathToJavaPackage(Path path){
+    static String pathToJavaPackage(Path path) {
         return path.toString().replace(File.separator, ".");
     }
 
-
-
-    static void addEntry(JarOutputStream jos, Path relativePath, byte[] content)  {
-            try {
-                var entry = new JarEntry(relativePath.toString());
-                jos.putNextEntry(entry);
-                jos.write(content);
-                jos.closeEntry();
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to add file to JAR: " + relativePath, e);
-            }
-        
+    static void addEntry(JarOutputStream jos, Path relativePath, byte[] content) {
+        try {
+            var entry = new JarEntry(relativePath.toString());
+            jos.putNextEntry(entry);
+            jos.write(content);
+            jos.closeEntry();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to add file to JAR: " + relativePath, e);
+        }
     }
-
 
     static void addEntry(Path rootClassesDirectory, JarOutputStream jarOutputStream, Path path) {
         var relativePath = rootClassesDirectory.relativize(path);
@@ -66,6 +67,5 @@ public interface Packer {
         } catch (IOException e) {
             throw new RuntimeException("Failed to add file to JAR: " + path, e);
         }
-
     }
 }
